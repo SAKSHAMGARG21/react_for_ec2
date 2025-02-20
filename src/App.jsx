3d1345310ca04fa5ce2c260a9680a62274a84ded
SAKSHAMGARG21 <sakshamgarggarg83@gmail.com>
2025-02-20 21:14:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hellow Aws Ec2 is Going On</h1>
+      <h2>My ReactJs app is working</h2>
+      <p>Deploy by Saksham Garg</p>
+    </div>
+  )
+}
+
+export default App
